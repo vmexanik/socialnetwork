20506c6cf1079be21afb262e6a2e6a223097f824
vmexanik <vmexanik1@gmail.com>
2019-04-15 16:45:08 +0300

    modified:   banProfile.php

--- banProfile.php
+++ banProfile.php
@@ -17,29 +17,40 @@ if (isset($_POST['banDate']))
 		header ('Location: main.php');
 }
 
+if ($_GET['ban']==0)
+{
+    $query="UPDATE users SET banned='0', banDate='0000-00-00' WHERE id='$id'";
+    $result=mysqli_query($link, $query) or exit(mysqli_error($link));
+    $_SESSION['message']='Аккаунт разбанен<p>';
+    $_GET['ban']==null;
+    header ('Location: main.php');
+}
+
 $sidebar='';
 
-if ($_SESSION['auth']==true){ //проверка авториазции
-		$header='Вы вошли как: '.$_SESSION['user'].'<p>';
-		$sidebar.='<a href=main.php>Главная</a><br>';
-		$sidebar.='<a href=logout.php>Выйти</a><br>';
+if ($_SESSION['auth']==true)
+{ //проверка авториазции
+    $header='Вы вошли как: '.$_SESSION['user'].'<p>';
+    $sidebar.='<a href=main.php>Главная</a><br>';
+    $sidebar.='<a href=logout.php>Выйти</a><br>';
 
-if ($_SESSION['status']=='admin')
-{
-	$content=	"ID записи:{$data['id']}<br>
-				Логин:{$data['login']}<br>
-				Дата рождения:{$data['birthday']} ({$data['age']} лет)<br>
-				Электронная почта:{$data['email']}<br>";
-	
-	$content.= 'Забанить до:
-	<form method="POST">
-	<input type="date" name="banDate" min="'.date('Y-m-d').'">
-	<input type="submit" value="Забанить">
-	</form>';
-	
-	
+    if ($_SESSION['status']=='admin')
+    {
+        $content=	"ID записи:{$data['id']}<br>
+                    Логин:{$data['login']}<br>
+                    Дата рождения:{$data['birthday']} ({$data['age']} лет)<br>
+                    Электронная почта:{$data['email']}<p>";
 
-}
+        $content.= 'Забанить до:';
+        $form= new Form();
+        $content.=$form->open(['method'=>'POST']);
+        $content.=$form->input(['type'=>'date', 'name'=>'banDate', 'min'=>'date("Y-m-d")']);
+        $content.=$form->input(['type'=>'submit', 'value'=>'Забанить']);
+        $content.='<p>';
+        $content.='<a href=banProfile.php?id='.$data['id'].'&ban=0>Разбанить</a>';
+        $content.=$form->close();
+
+    }
 }else {
 		$content= '<a href=auth.php>Пожалуйста, авторизуйтесь</a>';
 		}
